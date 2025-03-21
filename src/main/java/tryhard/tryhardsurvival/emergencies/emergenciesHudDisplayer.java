e8b6b2692fa9fe22c1b4852ca71b31d2b7004d55
Dave <newdevdave@gmail.com>
2025-03-21 11:37:32 -0400

    emergencies implementation (subject to change)

--- src/main/java/tryhard/tryhardsurvival/emergencies/emergenciesHudDisplayer.java
+++ src/main/java/tryhard/tryhardsurvival/emergencies/emergenciesHudDisplayer.java
@@ -1,4 +1,16 @@
 package tryhard.tryhardsurvival.emergencies;
 
-public class emergenciesHudDisplayer {
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.Identifier;
+
+public class emergenciesHudDisplayer implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
+
