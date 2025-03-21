58118618ea0b6640f893d4c776868bb1dfc37aa4
Dave <newdevdave@gmail.com>
2025-03-21 14:31:34 -0400

    yus

--- src/main/java/tryhard/tryhardsurvival/emergencies/emergenciesHudDisplayer.java
+++ src/main/java/tryhard/tryhardsurvival/emergencies/emergenciesHudDisplayer.java
@@ -1,5 +1,11 @@
 package tryhard.tryhardsurvival.emergencies;
 
+import net.minecraft.util.Identifier;
+import tryhard.tryhardsurvival.TryhardSurvival;
+
 public class emergenciesHudDisplayer {
+    private final Identifier[] emergencies = {
+            Identifier.of(TryhardSurvival.MOD_ID,"/textures/hud/emergency1.png")
+    };
 
 }
