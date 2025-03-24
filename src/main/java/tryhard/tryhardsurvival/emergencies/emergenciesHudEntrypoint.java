e2d3ea7e51b0e3fd8392654e7c9bd488cf0ed44e
Dave <newdevdave@gmail.com>
2025-03-24 13:25:19 -0400

    error fixing

--- src/main/java/tryhard/tryhardsurvival/emergencies/emergenciesHudEntrypoint.java
+++ src/main/java/tryhard/tryhardsurvival/emergencies/emergenciesHudEntrypoint.java
@@ -3,7 +3,6 @@ package tryhard.tryhardsurvival.emergencies;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.gui.DrawContext;
@@ -21,7 +20,6 @@ public final class emergenciesHudEntrypoint implements ClientModInitializer {
             Identifier.of("tryhardsurvival", "/textures/hud/emergency3.png")
     };
 
-
     @Override
     public void onInitializeClient() {
         threatScoreMgmt.init();
@@ -35,23 +33,25 @@ public final class emergenciesHudEntrypoint implements ClientModInitializer {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("dangerscore")
             .executes(context -> {
                 int threatScore = threatScoreMgmt.getThreatScore();
-                Formatting formatColor = Formatting.WHITE;
-
-                Text message = Text.literal("null");
-                if (emergencyLvl == 0) {
-                    message = Text.literal("<Angela>: The facility is at safety, for now.")
-                            .formatted(formatColor)
-                            .styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                                    Text.literal("Threat score detection sys"))));
-                } else if (emergencyLvl > 0) {
-                    if (emergencyLvl == 1) { formatColor = Formatting.YELLOW; }
-                    else if (emergencyLvl == 2 || emergencyLvl == 3) { formatColor = Formatting.RED; }
-                    message = Text.literal("<Angela>: The current threat score is: " + threatScore)
-                            .formatted(formatColor)
-                            .styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                                    Text.literal("Threat score detection sys"))));
-                }
-
+//                Formatting formatColor = Formatting.WHITE;
+//
+//                Text message = Text.literal("null");
+//                if (emergencyLvl == 0) {
+//                    message = Text.literal("<Angela>: The facility is at safety, for now.")
+//                            .formatted(formatColor)
+//                            .styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+//                                    Text.literal("Threat score detection sys"))));
+//                } else if (emergencyLvl > 0) {
+//                    if (emergencyLvl == 1) { formatColor = Formatting.YELLOW; }
+//                    else if (emergencyLvl == 2 || emergencyLvl == 3) { formatColor = Formatting.RED; }
+//                    message = Text.literal("<Angela>: The current threat score is: " + threatScore)
+//                            .formatted(formatColor)
+//                            .styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+//                                    Text.literal("Threat score detection sys"))));
+//                }
+//
+//                context.getSource().sendFeedback(message);
+                Text message = Text.literal(String.join("Danger score:",String.valueOf(threatScore),"\nEmergency level:",String.valueOf(emergencyLvl)));
                 context.getSource().sendFeedback(message);
                 return 1;
             })));
