4edffd6b9d3c781d16a663681facd187a7fd5092
Dave <newdevdave@gmail.com>
2025-03-26 14:24:59 -0400

    fixing hud render

--- src/main/java/tryhard/tryhardsurvival/emergencies/emergenciesHudRenderer.java
+++ src/main/java/tryhard/tryhardsurvival/emergencies/emergenciesHudRenderer.java
@@ -1,5 +1,6 @@
 package tryhard.tryhardsurvival.emergencies;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
@@ -30,12 +31,19 @@ public class emergenciesHudRenderer {
         int screenWidth = context.getScaledWindowWidth();
         int screenHeight = context.getScaledWindowHeight();
 
-        // Draw full-screen image
-        context.drawTexture(RenderLayer::getGuiTextured, trumpets[emergencyLvlMgmt.getEmergencyLvl() - 1],
-                0, 0,  // X, Y position (top-left corner)
-                0, 0,  // Texture X, Texture Y
-                screenWidth, screenHeight,  // Width and height (fill the screen)
-                screenWidth, screenHeight); // Texture width and height (scale to fit)
+        // apply 25% transparency / 75% visibility (0.75F)
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 0.75F);
+
+        // always on top
+        context.getMatrices().push();
+        context.getMatrices().translate(0, 0, 1000);
+
+        context.drawTexture(RenderLayer::getGuiTextured, trumpets[emergencyLvlMgmt.getEmergencyLvl() - 1], 0, 0, 0, 0, screenWidth, screenHeight, screenWidth, screenHeight);
+
+        context.getMatrices().pop();
+        RenderSystem.disableBlend();
     }
 
 }
